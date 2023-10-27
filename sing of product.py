e489a4dc462ba82a043eaeac3802a8123e1854d1
Mariem_AMZIL <147107786+MAR-AM@users.noreply.github.com>
2023-10-28 00:13:11 +0100

    (✿◡‿◡)

---
+++ sing of product.py
@@ -0,0 +1,9 @@
+m=float(input('Enter the first number : '))
+n=float(input('Enter the second number : '))
+if m*n>0:
+  print('the product of these two numbers is positive .')
+elif m*n<0:
+ print(' the product of these two numbers is negative .')
+elif m==0 or n==0 :
+  print(' the product of these two numbers is zero .')
+
